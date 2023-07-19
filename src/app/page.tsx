72864d8e4262598a562310849f953e5c42da10dc
Theophilus <isahtheophilus@gmail.com>
2023-07-19 14:56:39 +0100

    chore: use cheerio to parse xml data

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
-import { fetchGithubData } from "@/helpers/github";
+import { convertXmlToText } from "@/helpers/github";
 
 export default async function Home() {
-  const data = await fetchGithubData("lightning-dev/April_2016/000507_Acknowledgements-in-BOLT-2.xml");
-  console.log(data);
+  const d = await convertXmlToText()
+  console.log({d});
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       tldr mailing list
