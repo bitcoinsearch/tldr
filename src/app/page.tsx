d181a70bd95ffe8aa8cebc72261fdf943210bc0a
Theophilus <isahtheophilus@gmail.com>
2023-07-24 01:24:57 +0100

    feat: add cron action script to update search-indes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,7 @@
-import { indexAndSearch } from "../helpers/search-data";
 import FakeData from "../../data.json";
 import Homepage from "@/components/client/homepage";
-import { SearchDataParams } from "@/helpers/types";
 
 export type FakeDataType = typeof FakeData;
-
-async function getSearchData({ path, query }: SearchDataParams) {
-  const directory = `public/static/static${path ? "/" + path : "/"}`;
-  const data = await indexAndSearch(directory, query);
-  if (data) {
-    return data;
-  } else {
-    throw new Error("No data found");
-  }
-}
 
 export default async function Home() {
   const data = FakeData;
