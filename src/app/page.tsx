5dc6444abeb633ed55749766e46d0b5c2317edab
Emmanuel-Develops <itakpeemma@gmail.com>
2023-07-25 18:22:42 +0100

    feat: switch datasource to homepage.json

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,22 @@
-import FakeData from "../../data.json";
+import { indexAndSearch } from "../helpers/search-data";
 import Homepage from "@/components/client/homepage";
+import { HomepageData, SearchDataParams } from "@/helpers/types";
+import * as fs from "fs"
 
-export type FakeDataType = typeof FakeData;
+async function getHomepageData() {
+  try {
+    const data = fs.readFileSync("public/static/static/homepage.json", "utf-8")
+    const parsedData = JSON.parse(data) as HomepageData
+    return parsedData;
+  } catch (err) {
+    return null
+  }
+}
 
 export default async function Home() {
-  const data = FakeData;
+  const data = await getHomepageData()
+
+  if (!data) return null;
 
   return <Homepage data={data} />;
 }
--- src/helpers/types.ts
+++ src/helpers/types.ts
@@ -37,10 +37,28 @@ type SearchDataParams = {
   };
 };
 
+type HomepageEntryData = {
+  id: string;
+  title: string;
+  link: string;
+  authors: string[];
+  published_at: string;
+  summary: string;
+  n_threads: number;
+  dev_name: string;
+  contributors: string[];
+};
+
+type HomepageData = {
+  header_summary: string;
+  enteries: HomepageEntryData[];
+};
+
 export type {
   Feed,
   EntryData,
   MailingListType,
   SearchIndexData,
   SearchDataParams,
+  HomepageData
 };
