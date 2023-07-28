9514af5565640dca4849ca3205dac9275d6c685d
Theophilus <isahtheophilus@gmail.com>
2023-07-28 20:55:34 +0100

    chore: clean up code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,20 +1,22 @@
-import { indexAndSearch } from "../helpers/search-data";
 import Homepage from "@/components/client/homepage";
-import { HomepageData, SearchDataParams } from "@/helpers/types";
-import * as fs from "fs"
+import { HomepageData } from "@/helpers/types";
+import * as fs from "fs";
 
 async function getHomepageData() {
   try {
-    const data = fs.readFileSync("public/static/static/homepage.json", "utf-8")
-    const parsedData = JSON.parse(data) as HomepageData
+    const data = fs.readFileSync(
+      `${process.cwd()}/public/static/static/homepage.json`,
+      "utf-8"
+    );
+    const parsedData = JSON.parse(data) as HomepageData;
     return parsedData;
   } catch (err) {
-    return null
+    return null;
   }
 }
 
 export default async function Home() {
-  const data = await getHomepageData()
+  const data = await getHomepageData();
   if (!data) return null;
 
   return <Homepage data={data} />;
