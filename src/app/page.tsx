be8e19d481852c283f75ba89d29a027a49985926
IgboPharaoh <solomoneze62@gmail.com>
2023-10-06 08:17:18 +0100

    fix: remove months appearing in current batch even if presennt

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import Homepage from "@/app/components/client/homepage";
 import { readStaticDir } from "@/helpers/search-data";
 import { HomepageData, HomepageEntryData } from "@/helpers/types";
-import { flattenEntries, groupDuplicates, createArticlesFromFolder, groupAccordingToYears, sortAccordingToMonths } from "@/helpers/utils";
+import {
+  flattenEntries,
+  groupDuplicates,
+  createArticlesFromFolder,
+  groupAccordingToYears,
+  sortAccordingToMonths,
+  getBatchesInSameMonth,
+} from "@/helpers/utils";
 import * as fs from "fs";
 
 async function getHomepageData() {
@@ -19,29 +26,31 @@ const fetchDataInBatches = async (count: number): Promise<{ batch: HomepageEntry
   const folders = ["lightning-dev", "bitcoin-dev"];
   let result: HomepageEntryData[] = [];
 
-  await Promise.allSettled(
+  await Promise.all(
     folders.map(async (folder) => {
       if (!folder) return [];
 
       const DIRECTORY = `${process.cwd()}/public/static/static/${folder}`;
-      const files = fs.readdirSync(DIRECTORY).reverse();
+      const files = fs.readdirSync(DIRECTORY);
 
       const groupYears = groupAccordingToYears(files);
       const getGroupedYears = Object.values(groupYears) as Array<Array<string>>;
 
       const sortYears = sortAccordingToMonths(getGroupedYears);
       const dir = sortYears[count];
-
       const path = DIRECTORY + `/${dir}`;
+
       const folderData = await readStaticDir(path);
       const data = createArticlesFromFolder(folderData, folder);
+      const isSameMonth = getBatchesInSameMonth(data, dir);
 
-      result.push(...data);
+      result.push(...isSameMonth);
     })
   );
 
   const groups = groupDuplicates(result);
   const entries = Object.values(groups) as Array<HomepageEntryData[]>;
+
   const batch = flattenEntries(entries);
 
   return { batch, count };
