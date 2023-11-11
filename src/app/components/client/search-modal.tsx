3286f9774790403d7c8a76547d4986b472242c46
Theophilus <isahtheophilus@gmail.com>
2023-11-11 02:47:03 +0100

    chore: switch position of search bar

--- src/app/components/client/search-modal.tsx
+++ src/app/components/client/search-modal.tsx
@@ -4,17 +4,14 @@ import Image from "next/image";
 import React from "react";
 import { useDebouncedCallback } from "use-debounce";
 
-import {
-  MailingListType,
-  SearchDataParams,
-} from "@/helpers/types";
+import { MailingListType, SearchDataParams } from "@/helpers/types";
 import { urlMapping } from "@/config/config";
 import * as Dialog from "@radix-ui/react-dialog";
-import { ArrowDownIcon, ArrowUpIcon, Cross2Icon } from "@radix-ui/react-icons";
+import { Cross2Icon } from "@radix-ui/react-icons";
 
-import { defaultFilter, filterReducer } from "./actions/filter-reducer";
 import SearchResult from "./search-result-ui";
 import Spinner from "./spinner";
+import { useMediaQuery } from "./hooks/use-media-query";
 
 import { useSearch } from "@/services/search-service";
 import {
@@ -35,15 +32,18 @@ const SearchBox = () => {
   const [searchQuery, setSearchQuery] = React.useState<SearchDataParams | null>(
     null
   );
+  const isMobile = useMediaQuery("(max-width: 768px)");
 
+  const inputContainerRef = React.useRef<HTMLDivElement>(null);
   const searchFeedInputRef = React.useRef<HTMLInputElement>(null);
 
-  const { data, fetchNextPage, isFetching, isFetchingNextPage, isError } = useSearch({
-    queryString: searchQuery?.query.keyword ?? "",
-    authorString: searchQuery?.query.author ?? "",
-    mailListType: searchQuery?.path ?? null,
-    sortFields: [],
-  });
+  const { data, fetchNextPage, isFetching, isFetchingNextPage, isError } =
+    useSearch({
+      queryString: searchQuery?.query.keyword ?? "",
+      authorString: searchQuery?.query.author ?? "",
+      mailListType: searchQuery?.path ?? null,
+      sortFields: [],
+    });
 
   let results: SearchResults = {
     searchResults: [],
@@ -79,21 +79,19 @@ const SearchBox = () => {
     !isFetching &&
     !isFetchingNextPage &&
     !isError &&
-    Boolean(
-      results?.totalSearchResults && results?.totalSearchResults > 0
-    );
+    Boolean(results?.totalSearchResults && results?.totalSearchResults > 0);
 
   const showSpinner = (isFetching || isFetchingNextPage) && !isError;
 
   const setSearchQueryPath = (path: MailingListType | null) => {
-    const managePathSelection = (prevPath: MailingListType | null | undefined) => {
+    const managePathSelection = () => {
       if (searchQuery?.path === path) {
-        return null
+        return null;
       }
-      return path
-    }
+      return path;
+    };
     setSearchQuery((prev) => ({
-      path: managePathSelection(prev?.path),
+      path: managePathSelection(),
       query: {
         ...prev?.query,
       },
@@ -102,17 +100,6 @@ const SearchBox = () => {
 
   const resetToDefault = () => {
     setSearchQuery(null);
-  };
-
-  const setRelevance = (type: "old-new" | "new-old") => {
-    setSearchQuery((prev) => ({
-      path: prev?.path ?? null,
-      query: {
-        keyword: prev?.query.keyword,
-        author: prev?.query.author,
-      },
-      relevance: type,
-    }));
   };
 
   const debouncedSearch = useDebouncedCallback((value) => {
@@ -162,149 +149,145 @@ const SearchBox = () => {
   }, [open]);
 
   return (
-    <Dialog.Root open={open} onOpenChange={setOpen}>
-      <Dialog.Trigger asChild>
-        <div className="relative">
-          <input
-            name="searchbox"
-            className="border-[1px] rounded-md border-[#ccc] w-[200px] md:w-[274px] py-2 pl-9"
-            placeholder="search [cmd/ctrl+k]"
-          />
-          <Image
-            className="absolute top-1/2 left-0 -translate-y-1/2 ml-2 pointer-events-none"
-            src="/icons/search_icon.svg"
-            alt="search_icon"
-            width={24}
-            height={24}
-          />
-        </div>
-      </Dialog.Trigger>
-      <Dialog.Portal>
-        <Dialog.Overlay className="bg-slate-400 opacity-50 data-[state=open]:animate-overlayShow fixed inset-0 z-20" />
-        <Dialog.Content className="data-[state=open]:animate-contentShow overflow-y-auto fixed top-[50%] left-[50%] max-h-[65vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none z-30">
-          <Dialog.Title className="m-0 text-[16px] font-medium flex items-center gap-x-4">
-            <span>Filter results by: </span>
-            <span className="flex items-center gap-x-2">
-              <button
-                className={`border rounded py-1 px-2 text-xs hover:bg-slate-100 ${
-                  searchQuery?.path === BITCOINDEV && "bg-slate-200"
-                }`}
-                onClick={() => {
-                  setSearchQueryPath(BITCOINDEV);
-                }}
-              >
-                bitcoin-dev
-              </button>
-              <button
-                className={`border rounded py-1 px-2 text-xs hover:bg-slate-100 ${
-                  searchQuery?.path === LIGHTNINGDEV && "bg-slate-200"
-                }`}
-                onClick={() => {
-                  setSearchQueryPath(LIGHTNINGDEV);
-                }}
-              >
-                lightning-dev
-              </button>
-              <button
-                className={`border p-[6px] hover:bg-slate-100 focus:bg-slate-100 inline-flex h-[25px] w-[25px] appearance-none items-center justify-center focus:outline-none`}
-                aria-label="clear-filter"
-                onClick={() => {
-                  setSearchQueryPath(null);
-                }}
-              >
-                <Cross2Icon />
-              </button>
-            </span>
-          </Dialog.Title>
-          <Dialog.Description
-            className={`flex justify-between mt-[10px] mb-5 text-[15px] leading-normal ${
-              showDescription ? "visible" : "hidden"
-            }`}
-          >
-            <div>
-              Search results for{" "}
-              {searchQuery?.query.keyword && (
-                <span className="font-medium italic">
-                  {searchQuery.query.keyword}
-                </span>
-              )}
-              {searchQuery?.query.author && (
-                <span>
-                  {" "}
-                  {searchQuery?.query.keyword && "by"}{" "}
-                  <span className="font-medium italic">
-                    {searchQuery.query.author}
-                  </span>
-                </span>
-              )}
-            </div>
-            {/* <div className="font-medium flex gap-x-3 text-sm text-right items-center justify-center">
-              <span>relevance</span>
-              <div className="flex items-center gap-x-1">
-                <button
-                  className={`${
-                    filter.old && "bg-slate-200 border-slate-500"
-                  }}`}
-                  onClick={() => {
-                    dispatch({ type: "old-new" });
-                    setRelevance("old-new");
-                  }}
-                >
-                  <ArrowUpIcon />
-                </button>
-                <button
-                  className={`${
-                    filter.new && "bg-slate-200 border-slate-500"
-                  }}`}
-                  onClick={() => {
-                    dispatch({ type: "new-old" });
-                    setRelevance("new-old");
-                  }}
-                >
-                  <ArrowDownIcon />
-                </button>
-              </div>
-            </div> */}
-          </Dialog.Description>
-          <fieldset className="my-[15px] flex flex-col items-start gap-2">
-            <label htmlFor="search"></label>
+    <div className="relative" ref={inputContainerRef}>
+      <Dialog.Root open={open} onOpenChange={setOpen}>
+        <Dialog.Trigger asChild>
+          <div className="relative">
             <input
-              className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] p-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_1.5px]"
-              id="search"
-              ref={searchFeedInputRef}
-              placeholder="Search feed"
-              onChange={(e) => debouncedSearch(e.target.value)}
+              name="searchbox"
+              className="border-[1px] rounded-md border-[#ccc] w-[200px] md:w-[274px] py-2 pl-9"
+              placeholder="search [cmd/ctrl+k]"
             />
-            <label htmlFor="search-author"></label>
-            <input
-              className="inline-flex h-[35px] w-[50%] flex-1 items-center justify-center rounded-[4px] p-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
-              id="search-author"
-              placeholder="Optional: author name"
-              onChange={(e) => debouncedSearchAuthor(e.target.value)}
-            />
-          </fieldset>
-          <div className="mt-[25px] flex flex-col justify-center items-center">
-            <Spinner isPending={showSpinner} />
-            <SearchResult
-              searchResults={results}
-              searchQuery={searchQuery}
-              isPending={isFetching || isFetchingNextPage}
-              showMoreResults={handleFetchMore}
-              setOpen={setOpen}
+            <Image
+              className="absolute top-1/2 left-0 -translate-y-1/2 ml-2 pointer-events-none"
+              src="/icons/search_icon.svg"
+              alt="search_icon"
+              width={24}
+              height={24}
             />
           </div>
-          <Dialog.Close asChild>
-            <button
-              className="text-[10px] hover:bg-slate-300 bg-slate-200 focus:shadow-slate-300 absolute top-[10px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded focus:outline-none"
-              aria-label="Close"
-              onClick={resetToDefault}
+        </Dialog.Trigger>
+        <Dialog.Portal>
+          <Dialog.Overlay className="bg-slate-400 opacity-50 lg:bg-none lg:opacity-0 data-[state=open]:animate-overlayShow fixed inset-0 z-20" />
+          <Dialog.Content
+            className={`dialogContent fixed rounded-[5px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none overflow-y-auto ${
+              isMobile
+                ? "mx-auto my-0 top-[50%] left-[50%] max-h-[65vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%]"
+                : "absolute left-[48.5%]"
+            } p-4 bg-white shadow-xl z-20 ${
+              open ? "animate-slideIn" : "animate-slideOut"
+            }`}
+            style={{
+              top: isMobile
+                ? "50%"
+                : `calc(${
+                    inputContainerRef.current?.getBoundingClientRect().bottom
+                  }px + 10px)`,
+              transform: isMobile ? "translate(-50%, -50%)" : "none",
+              width: isMobile ? "90vw" : "70vw",
+              maxWidth: isMobile ? "450px" : "420px",
+              maxHeight: isMobile ? "60vh" : "calc(70vh - 100px)",
+            }}
+          >
+            <Dialog.Title className="m-0 text-[16px] font-medium flex items-center gap-x-4">
+              <span>Filter results by: </span>
+              <span className="flex items-center gap-x-2">
+                <button
+                  className={`border rounded py-1 px-2 text-xs hover:bg-slate-100 ${
+                    searchQuery?.path === BITCOINDEV && "bg-slate-200"
+                  }`}
+                  onClick={() => {
+                    setSearchQueryPath(BITCOINDEV);
+                  }}
+                >
+                  bitcoin-dev
+                </button>
+                <button
+                  className={`border rounded py-1 px-2 text-xs hover:bg-slate-100 ${
+                    searchQuery?.path === LIGHTNINGDEV && "bg-slate-200"
+                  }`}
+                  onClick={() => {
+                    setSearchQueryPath(LIGHTNINGDEV);
+                  }}
+                >
+                  lightning-dev
+                </button>
+                <button
+                  className={`border p-[6px] hover:bg-slate-100 focus:bg-slate-100 inline-flex h-[25px] w-[25px] appearance-none items-center justify-center focus:outline-none`}
+                  aria-label="clear-filter"
+                  onClick={() => {
+                    setSearchQueryPath(null);
+                  }}
+                >
+                  <Cross2Icon />
+                </button>
+              </span>
+            </Dialog.Title>
+            <Dialog.Description
+              className={`flex justify-between mt-[10px] mb-5 text-[15px] leading-normal ${
+                showDescription ? "visible" : "hidden"
+              }`}
             >
-              esc
-            </button>
-          </Dialog.Close>
-        </Dialog.Content>
-      </Dialog.Portal>
-    </Dialog.Root>
+              <div>
+                Search results for{" "}
+                {searchQuery?.query.keyword && (
+                  <span className="font-medium italic">
+                    {searchQuery.query.keyword}
+                  </span>
+                )}
+                {searchQuery?.query.author && (
+                  <span>
+                    {" "}
+                    {searchQuery?.query.keyword && "by"}{" "}
+                    <span className="font-medium italic">
+                      {searchQuery.query.author}
+                    </span>
+                  </span>
+                )}
+              </div>
+            </Dialog.Description>
+            <fieldset className="my-[15px] flex flex-col items-start gap-2">
+              <label htmlFor="search"></label>
+              <input
+                className="inline-flex h-[35px] w-full flex-1 items-center justify-center rounded-[4px] p-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_1.5px]"
+                id="search"
+                ref={searchFeedInputRef}
+                placeholder="Search feed"
+                onChange={(e) => debouncedSearch(e.target.value)}
+              />
+              <label htmlFor="search-author"></label>
+              <input
+                className="inline-flex h-[35px] w-[50%] flex-1 items-center justify-center rounded-[4px] p-[10px] text-[15px] leading-none shadow-[0_0_0_1px] outline-none focus:shadow-[0_0_0_2px]"
+                id="search-author"
+                placeholder="Optional: author name"
+                onChange={(e) => debouncedSearchAuthor(e.target.value)}
+              />
+            </fieldset>
+            <div className="mt-[25px] flex flex-col justify-center items-center">
+              <Spinner isPending={showSpinner} />
+              <SearchResult
+                searchResults={results}
+                searchQuery={searchQuery}
+                isPending={isFetching || isFetchingNextPage}
+                showMoreResults={handleFetchMore}
+                setOpen={setOpen}
+              />
+            </div>
+            <Dialog.Close asChild>
+              <button
+                className={`${
+                  isMobile && "hidden"
+                } text-[10px] hover:bg-slate-300 bg-slate-200 focus:shadow-slate-300 absolute top-[16px] right-[10px] inline-flex h-[25px] w-[25px] appearance-none items-center justify-center rounded focus:outline-none`}
+                aria-label="Close"
+                onClick={resetToDefault}
+              >
+                esc
+              </button>
+            </Dialog.Close>
+          </Dialog.Content>
+        </Dialog.Portal>
+      </Dialog.Root>
+    </div>
   );
 };
 
