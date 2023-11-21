79812d5a492ac3b1903b800ede8146325fbbcc56
IgboPharaoh <solomoneze62@gmail.com>
2023-11-21 16:19:38 +0100

    add menu to navbar

--- src/app/components/server/navbar.tsx
+++ src/app/components/server/navbar.tsx
@@ -4,26 +4,24 @@ import React from "react";
 import SearchBox from "../client/search-modal";
 import TanstackProvider from "@/app/provider";
 import Image from "next/image";
+import Menu from "../client/navigation-menu";
 
 const Navbar = () => {
   return (
-    <div className="w-full max-w-3xl mx-auto mt-2">
-      <Link href="/rss.xml" target="_blank" about="tldr-rss-feed">
-        <Image
-          src="/icons/feed_icon.svg"
-          alt="tldr-rss-feed"
-          width={22}
-          height={22}
-          className="mb-1"
-        />
+    <div className='w-full max-w-3xl mx-auto mt-2'>
+      <Link href='/rss.xml' target='_blank' about='tldr-rss-feed'>
+        <Image src='/icons/feed_icon.svg' alt='tldr-rss-feed' width={22} height={22} className='mb-1' />
       </Link>
-      <div className="flex justify-between items-center">
-        <Link href="/">
-          <p className="text-xl md:text-4xl font-[600]">Bitcoin TLDR</p>
+      <div className='flex justify-between items-center'>
+        <Link href='/'>
+          <p className='text-xl md:text-4xl font-[600]'>Bitcoin TLDR</p>
         </Link>
-        <TanstackProvider>
-          <SearchBox />
-        </TanstackProvider>
+        <div className='flex gap-4 items-center justify-center'>
+          <TanstackProvider>
+            <SearchBox />
+          </TanstackProvider>
+          <Menu />
+        </div>
       </div>
     </div>
   );
