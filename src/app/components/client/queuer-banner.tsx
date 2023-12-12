2fa1c33fea0e501d22f18d2480712435bfa20732
Emmanuel-Develops <itakpeemma@gmail.com>
2023-12-12 11:06:41 +0100

    feat: queuer banner

---
+++ src/app/components/client/queuer-banner.tsx
@@ -0,0 +1,54 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import React, { useState, useEffect } from "react";
+import styles from "./banner.module.css";
+
+const BANNER_KEY = "queuer-banner";
+
+const QueuerBanner = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = () => {
+    if (typeof window !== "undefined") {
+      sessionStorage.setItem(BANNER_KEY, "hidden");
+    }
+    setIsOpen(false);
+  };
+
+  useEffect(() => {
+    const banner_in_session = window.sessionStorage.getItem(BANNER_KEY);
+    if (banner_in_session === "hidden") {
+      setIsOpen(false);
+    } else {
+      setIsOpen(true)
+    }
+  }, []);
+
+  // if (queuerBanner == "hidden") return null;
+  if (!isOpen) return null;
+  return (
+    <div className="bg-orange-100 flex items-center w-full sticky top-20 -translate-y-20 z-[99]">
+      <div className="flex grow items-center text-sm md:text-base justify-between px-2 md:px-4">
+        <div className="text-gray-600 flex-[1_1_100%] font-medium underline text-center">
+          <Link href="https://review.btctranscripts.com/transcripts" target="_blank" className="text-center">
+            <span>{`Get `}</span>
+            <span
+              style={{
+                fontWeight: "medium",
+                textTransform: "uppercase",
+                color: "#ED8936",
+              }}
+            >{`paid `}</span>
+            <span>{`to review technical Bitcoin transcripts`}</span>
+          </Link>
+        </div>
+        <button onClick={handleClose} className="relative h-[18px] w-[18px] grid place-items-center rounded-full hover:bg-orange-300">
+          <Image src="/close_icon.svg" width={10} height={10} alt="close" />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default QueuerBanner;
