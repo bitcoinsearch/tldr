ae149e0b8988018e2eedaa2d950d4b94bf2f18de
IgboPharaoh <solomoneze62@gmail.com>
2024-01-09 15:10:05 +0100

    add config

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,5 +1,6 @@
 export const BITCOINDEV = "bitcoin-dev";
 export const LIGHTNINGDEV = "lightning-dev";
+export const DELVINGBITCOIN = "delvingbitcoin";
 export const BATCHSIZE = 3;
 export const DEBOUNCE_DELAY = 1200;
 export const DEFAULT_LIMIT_OF_RESULTS_TO_DISPLAY = 10;
