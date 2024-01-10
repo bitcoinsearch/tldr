c610174489af56b0c3ecbfcba8d58f984103b170
Theophilus <isahtheophilus@gmail.com>
2024-01-10 05:53:47 +0100

    fix: newsletter grouping function

--- src/app/components/server/newsletter.tsx
+++ src/app/components/server/newsletter.tsx
@@ -6,12 +6,17 @@ import { NewsLetter, NewsLetterDataType } from "@/helpers/types";
 
 export const NewsletterCard = ({ entry }: { entry: NewsLetter }) => {
   const publishedAtDateDisplay = formattedDate(entry.published_at);
-  const path = entry.combined_summ_file_path.length ? entry.combined_summ_file_path : entry.file_path;
+  const path = entry.combined_summ_file_path.length
+    ? entry.combined_summ_file_path
+    : entry.file_path;
 
   return (
-    <div key={`${entry.id}_${entry.title}`} className='flex flex-col gap-2'>
-      <p className='text-sm'>{publishedAtDateDisplay}</p>
-      <Link href={path} className='font-inika text-lg md:text-2xl underline cursor-pointer capitalize pb-3'>
+    <div key={`${entry.id}_${entry.title}`} className="flex flex-col gap-2">
+      <p className="text-sm">{publishedAtDateDisplay}</p>
+      <Link
+        href={path}
+        className="font-inika text-lg md:text-2xl underline cursor-pointer capitalize pb-3"
+      >
         {entry.title}
       </Link>
       <SummaryList summary={entry.summary} />
@@ -19,33 +24,47 @@ export const NewsletterCard = ({ entry }: { entry: NewsLetter }) => {
   );
 };
 
-export const NewsletterPage = ({ newsletter }: { newsletter: NewsLetterDataType }) => {
+export const NewsletterPage = ({
+  newsletter,
+}: {
+  newsletter: NewsLetterDataType;
+}) => {
   if (!newsletter) return null;
 
   return (
-      <div>
-        <section>
-          <h2 className='text-xl md:text-4xl font-normal pb-8 pt-10'>Summary</h2>
-          <p className='pb-12'>{newsletter.summary_of_threads_started_this_week}</p>
-        </section>
+    <div>
+      <section>
+        <h2 className="text-xl md:text-4xl font-normal pb-8 pt-10">Summary</h2>
+        <p className="pb-12">
+          {newsletter.summary_of_threads_started_this_week}
+        </p>
+      </section>
 
-        <section className='pb-12'>
-          <h2 className='text-xl md:text-4xl font-normal pb-8 pt-10'>New posts</h2>
-          <section className='flex flex-col gap-9'>
-            {newsletter.new_threads_this_week.map((entry) => (
-              <NewsletterCard entry={entry} key={entry.id} />
-            ))}
-          </section>
+      <section className="pb-12">
+        <h2 className="text-xl md:text-4xl font-normal pb-8 pt-10">
+          New posts
+        </h2>
+        <section className="flex flex-col gap-9">
+          {!newsletter.new_threads_this_week.length ? (
+            <p>Oops! No new posts this week. Explore the ongoing discussions below.</p>
+          ) : (
+            newsletter.new_threads_this_week.map((entry) => {
+              return <NewsletterCard entry={entry} key={entry.id} />;
+            })
+          )}
         </section>
+      </section>
 
-        <section className='pb-12'>
-          <h2 className='text-xl md:text-4xl font-normal pb-8 pt-10'>Ongoing Discussions</h2>
-          <section className='flex flex-col gap-9'>
-            {newsletter.active_posts_this_week.map((entry) => (
-              <NewsletterCard entry={entry} key={entry.id} />
-            ))}
-          </section>
+      <section className="pb-12">
+        <h2 className="text-xl md:text-4xl font-normal pb-8 pt-10">
+          Ongoing Discussions
+        </h2>
+        <section className="flex flex-col gap-9">
+          {newsletter.active_posts_this_week.map((entry) => (
+            <NewsletterCard entry={entry} key={entry.id} />
+          ))}
         </section>
-      </div>
+      </section>
+    </div>
   );
 };
