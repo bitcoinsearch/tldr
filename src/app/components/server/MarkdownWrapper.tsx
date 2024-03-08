6afd3106bbd201d1e9c70445a88ba358ca008e0a
IgboPharaoh <solomoneze62@gmail.com>
2024-03-08 08:01:11 +0100

    fix headline text not wrapped in markdown wrapper

--- src/app/components/server/MarkdownWrapper.tsx
+++ src/app/components/server/MarkdownWrapper.tsx
@@ -3,5 +3,14 @@ import ReactMarkdown from "react-markdown";
 import "../../globals.css";
 
 export const MarkdownWrapper = ({ summary, className }: { summary: string; className?: string | null | undefined }) => {
-  return <ReactMarkdown className={`markdownStyles ${className}`} >{summary}</ReactMarkdown>;
+  return (
+    <ReactMarkdown
+      components={{
+        a: (props) => <a {...props} className=' text-inherit' target='_blank' />,
+      }}
+      className={`markdownStyles ${className}`}
+    >
+      {summary}
+    </ReactMarkdown>
+  );
 };
