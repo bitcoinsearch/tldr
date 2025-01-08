414e3c2be6c9e2e2699b9fdb6c6b52e902313934
IgboPharaoh <solomoneze62@gmail.com>
2025-01-08 21:26:09 +0100

    Restructure page layout and add new font family

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,9 @@
-import Navbar from "@/app/components/server/navbar";
-import "@bitcoin-dev-project/bdp-ui/styles.css";
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter, Inika } from "next/font/google";
+import Footer from "./components/client/footer";
+import { Inter, Inika, IBM_Plex_Serif } from "next/font/google";
+import "@bitcoin-dev-project/bdp-ui/styles.css";
+import Navbar from "@/app/components/server/navbar";
 
 const inter = Inter({ subsets: ["latin"], variable: "--font-inter" });
 const inika = Inika({
@@ -13,12 +14,20 @@ const inika = Inika({
   variable: "--font-inika",
 });
 
+const ibmPlexSerif = IBM_Plex_Serif({
+  weight: ["400"],
+  style: ["normal"],
+  subsets: ["latin"],
+  display: "swap",
+  variable: "--font-ibm-plex-serif",
+});
+
 export const metadata: Metadata = {
   title: "Bitcoin TLDR",
   alternates: {
     types: {
-      'application/rss+xml': './rss.xml',
-    }
+      "application/rss+xml": "./rss.xml",
+    },
   },
   description: "Bitcoin-dev and Lightning-dev mailing list summaries and discoveries",
   keywords:
@@ -44,47 +53,17 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en' className='h-full'>
-      <body className={`${inter.variable} ${inika.variable} font-inter h-full`}>
-        <div className='pt-2 md:pt-[56px] w-full items-center h-full flex flex-col'>
+      <body className={`${inter.variable} ${inika.variable} ${ibmPlexSerif.variable} font-inter h-full`}>
+        <div className='w-full items-center h-full flex flex-col'>
           <div className='w-full grow flex flex-col'>
-            <div className='sticky top-0 z-50'>
-              <div className='sticky top-0 z-[99]'>
-                <div className='sticky top-0 bg-white w-full max-h-[98px] flex flex-col items-center p-2 md:p-4 px-4 z-[99] justify-center'>
-                  <Navbar />
-                </div>
-              </div>
+            <div className='z-10'>
+              <Navbar />
             </div>
-            <div className='w-full mx-auto grow max-w-3xl pb-8 px-4 lg:px-0'>{children}</div>
-            <footer className="p-[24px] bg-black dark:bg-black text-white text-center w-full">
-              <p style={{ fontSize: "16px", color: "lightgray" }}>
-                Built with ❤️ by{" "}
-                <a href='https://bitcoindevs.xyz' target='_blank' rel='noopener noreferrer' style={{ color: "orange" }}>
-                  The Bitcoin Dev Project
-                </a>
-              </p>
-              <a
-                href='https://cryptpad.fr/form/#/2/form/view/3P2CsohsHOkcH7C+WdtX0-tvqjBHqXnAmz5D9yx0e04/'
-                target='_blank'
-                rel='noopener noreferrer'
-                style={{ fontSize: "14px", color: "orange" }}
-              >
-                Submit Feedback
-              </a>
-              <p style={{ fontSize: "14px", color: "lightgray" }}>
-                Vistor counts publicly available via {" "}
-                <a
-                  href='https://visits.bitcoindevs.xyz/share/btDhVSkbULr146OJ/tldr'
-                  target='_blank'
-                  rel='noopener noreferrer'
-                  style={{ fontSize: "14px", color: "orange" }}
-                >
-                  umami
-                </a>
-              </p>
-            </footer>
+            <div>{children}</div>
+            <Footer />
           </div>
         </div>
-        <script async src="https://visits.bitcoindevs.xyz/script.js" data-website-id="a711d1d5-764e-461e-83bd-d715c5b8cb29"></script>
+        <script async src='https://visits.bitcoindevs.xyz/script.js' data-website-id='a711d1d5-764e-461e-83bd-d715c5b8cb29'></script>
       </body>
     </html>
   );
