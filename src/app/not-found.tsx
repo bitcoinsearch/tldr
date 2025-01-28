7c27acc275b82e1d25eafd0941f74be2009b2665
IgboPharaoh <solomoneze62@gmail.com>
2025-01-28 07:04:12 +0100

    chore: update custom not found page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,22 +1,49 @@
+"use client";
+
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+import Wrapper from "./components/server/wrapper";
+import { ArrowLinkRight } from "@bitcoin-dev-project/bdp-ui/icons";
 import Link from "next/link";
 
 export default function NotFound() {
+  const router = useRouter();
+
   return (
-    <main id='main' className='flex w-full items-center justify-center h-full pb-[60px]'>
-      <div className='w-full'>
-        <div className='flex flex-col items-center justify-center gap-y-6 w-full h-full'>
-          <h1 className='md:text-4xl font-bold text-center'>404 - Page Not Found</h1>
-          <p className='text-sm md:text-lg text-center max-w-xs md:max-w-lg'>
-            The page you are looking for might have been removed, had its name changed or is temporarily unavailable.
-          </p>
-          <Link
-            href='/'
-            className='flex justify-center items-center gap-2 py-3 px-4 font-bold mx-auto text-sm text-black lg:text-base bg-gray-200 disabled:bg-custom-hover-state disabled:cursor-not-allowed rounded-[10px]'
-          >
-            Go to Homepage
-          </Link>
+    <Wrapper>
+      <div className='h-[calc(100vh-81px)] flex flex-col'>
+        <Link
+          href='/'
+          className='flex text-sm md:text-base items-center gap-2 font-gt-walsheim border border-black rounded-full px-4 py-2 w-fit mt-6 md:mt-10'
+        >
+          <ArrowLinkRight className='text-black w-5 md:w-6 rotate-180' />
+          Return Home
+        </Link>
+
+        <div className='flex items-center justify-center h-full'>
+          <div className='flex flex-col items-center justify-center gap-6 max-h-[663px] sm:w-full h-full'>
+            <Image
+              src='/images/not-found-image.png'
+              alt='not found image'
+              height={663}
+              width={562}
+              className='h-[276px] md:h-[663px] w-[326px] md:w-[562px]'
+              priority
+            />
+
+            <section className='flex flex-col gap-4 md:gap-6 w-full items-center justify-center'>
+              <p className='text-2xl sm:text-[32px] lg:text-[48px] leading-[62.06px] font-test-signifier text-nowrap'>Page Not Found!</p>
+              <button
+                onClick={() => router.back()}
+                className='flex items-center justify-center gap-2 py-4 md:py-6 bg-orange-custom-100 rounded-full max-w-[126px] md:max-w-[214px] w-full text-white text-nowrap'
+              >
+                <ArrowLinkRight className='text-white w-5 md:w-6 rotate-180' />
+                Go Back
+              </button>
+            </section>
+          </div>
         </div>
       </div>
-    </main>
+    </Wrapper>
   );
 }
