3bc11740fe65966f2cee5e7d8e0fb4fbc810fb87
IgboPharaoh <solomoneze62@gmail.com>
2025-01-29 17:46:03 +0100

    initial scaffold of individual discussions page

--- src/app/summary/[...path]/page.tsx
+++ src/app/summary/[...path]/page.tsx
@@ -4,6 +4,10 @@ import DiscussionHistory from "./components/historythread";
 import Link from "next/link";
 import BreadCrumbs from "./components/BreadCrumb";
 import { MarkdownWrapper } from "@/app/components/server/MarkdownWrapper";
+import Wrapper from "@/app/components/server/wrapper";
+import { ArrowLeftIcon } from "@radix-ui/react-icons";
+import Image from "next/image";
+import { ThreadSummary } from "./components/thread-summary";
 
 const getSummaryData = async (path: string[]) => {
   const pathString = path.join("/");
@@ -19,6 +23,7 @@ const getSummaryData = async (path: string[]) => {
 export default async function Page({ params, searchParams }: { params: { path: string[] }; searchParams: { replies: string } }) {
   const summaryData = await getSummaryData(params.path);
   if (!summaryData) return <h1>No data found</h1>;
+
   const splitSentences = summaryData.data.entry.summary.split(/(?<=[.!?])\s+/);
   const firstSentence = splitSentences[0];
   const newSummary = summaryData.data.entry.summary.replace(firstSentence, "");
@@ -27,35 +32,70 @@ export default async function Page({ params, searchParams }: { params: { path: s
   const publishedAtDateDisplay = formattedDate(entry.published);
   const isCombinedPage = params.path[2].startsWith("combined");
 
+  const postBreadcrumbData = () => {
+    /**
+     * routes to account for:
+     * - Home
+     * - dev_name
+     * - topic
+     * - Thread Summary
+     * - Authors
+     */
+
+    const devName = params.path[0];
+
+    const routes = [
+      {
+        name: "Home",
+        link: "/",
+      },
+      {
+        name: devName,
+        link: `/posts?source=all-activity&dev=${devName}`,
+      },
+      {
+        name: "Thread Summary",
+        link: `/summary/${params.path.join("/")}`,
+      },
+    ];
+
+    return routes;
+  };
+
+  const routes = postBreadcrumbData();
+
   return (
-    <main className='max-w-[866px] mx-auto'>
-      <div className='flex flex-col gap-4 my-10'>
-        <BreadCrumbs params={params} summaryData={summaryData} replies={searchParams.replies} />
-        <h2 className='font-inika text-4xl'>{summaryData.data.title}</h2>
-        <div className={`flex flex-col`}>
-          <div className='flex items-center gap-2'>
-            {historyLinks && historyLinks?.length == 0 && link ? (
-              <Link href={link} target='_blank'>
-                <span className='pb-[2px] border-b-2 border-brand-secondary leading-relaxed text-brand-secondary font-semibold'>Original Post</span>
-              </Link>
-            ) : null}
-            {authors.length === 1 ? <span className='text-gray-600 font-semibold'>by {authors[0].name}</span> : null}
+    <Wrapper>
+      <div>
+        <div>
+          <div className='pt-5 md:pt-[54px]'>
+            <Link href='/' className='flex items-center gap-2 py-2 px-4 md:px-6 rounded-full border border-black w-fit'>
+              <ArrowLeftIcon className='w-5 h-5 md:w-6 md:h-6' strokeWidth={4} />
+              <span className='text-sm md:text-base font-medium md:font-normal font-gt-walsheim leading-[18.32px]'>Back to Active Discussions</span>
+            </Link>
           </div>
-          {isCombinedPage ? null : (
-            <h3 className=' text- font-medium pt-3'>
-              <span className=' mr-1 font- text-gray-600'>Posted on: </span>
-              {publishedAtDateDisplay}
-            </h3>
-          )}
+
+          {/* breadcrumb */}
+          <>
+            <section className='flex items-center py-6'>
+              {routes.map((link, index) => (
+                <div key={link.name} className='flex items-center'>
+                  <Link
+                    href={link.link}
+                    key={link.name}
+                    className='text-sm md:text-base font-medium md:font-normal font-gt-walsheim leading-[18.32px] text-gray-custom-1100'
+                  >
+                    {link.name}
+                  </Link>
+                  {index !== routes.length - 1 && <p className='text-gray-custom-1200 px-[5px]'>/</p>}
+                </div>
+              ))}
+            </section>
+          </>
         </div>
+
+        <ThreadSummary summaryData={summaryData} searchParams={searchParams} />
       </div>
-      <section className='my-10'>
-        <MarkdownWrapper summary={firstSentence} className='text-2xl font-inika my-2' />
-        <MarkdownWrapper summary={newSummary} className='whitespace-pre-line summaryTags' />
-      </section>
-      {historyLinks && historyLinks?.length > 0 ? (
-        <DiscussionHistory historyLinks={historyLinks} authors={authors} replies={searchParams.replies} />
-      ) : null}
-    </main>
+    </Wrapper>
   );
 }
