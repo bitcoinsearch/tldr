787597e0a2e7a867c02ab01dd00451f762d98ed0
Jonas <jonas@chaincode.com>
2026-01-06 12:52:03 -0500

    add better error handling

--- src/helpers/send-newsletter.ts
+++ src/helpers/send-newsletter.ts
@@ -544,7 +544,12 @@ const sendNewsletter = async (): Promise<void> => {
       fs.writeFileSync("./newsletter.html", htmlContent);
     }
   } catch (err: any) {
-    throw new Error(err);
+    if (err.response) {
+      console.error("Mailchimp Error:", err.response.body);
+    } else {
+      console.error("Error:", err);
+    }
+    process.exit(1);
   }
 };
 
